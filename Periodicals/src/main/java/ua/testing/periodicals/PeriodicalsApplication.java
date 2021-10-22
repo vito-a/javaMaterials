3f5e5a911e7e054fb56d7841086b2d0289edf16c
vito-a <vito_a@meta.ua>
2021-10-22 18:42:50 +0300

    Java Autumn 2021 - 19. Introduction to Database And SQL - Periodicals - javadoc documentation

--- Periodicals/src/main/java/ua/testing/periodicals/PeriodicalsApplication.java
+++ Periodicals/src/main/java/ua/testing/periodicals/PeriodicalsApplication.java
@@ -3,9 +3,17 @@ package ua.testing.periodicals;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The Periodicals application.
+ */
 @SpringBootApplication
 public class PeriodicalsApplication {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the application input arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(PeriodicalsApplication.class, args);
     }
