971872430889fe397d6efc97fed7264f4fb8b7da
vito-a <vito_a@meta.ua>
2022-03-25 20:04:07 +0200

    Java 2022 - Homework 24 - Hashcode and Equals - Checking the Case 2: two Items with the same values in a HashSet

--- Homework24/src/main/java/ua/training/Main.java
+++ Homework24/src/main/java/ua/training/Main.java
@@ -2,10 +2,30 @@ package ua.training;
 
 import ua.training.model.Item;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
-        Item item = new Item();
-        System.out.println(item.getClass());
+        Set<Item> testSet = new HashSet<>();
+
+        Item item1 = new Item();
+        item1.setId(1L);
+        item1.setName("Name1");
+        System.out.println(item1.getClass());
+
+        Item item2 = new Item();
+        item2.setId(1L);
+        item2.setName("Name1");
+
+        // Checking the Case 2: two Items with the same values are equal
+        // so in a HashSet only the first one inserted should exist
+        testSet.add(item1);
+        testSet.add(item2);
+
+        for (Item i : testSet) {
+            System.out.println("Item from set: ID=" + i.getId() + ", Name=" + i.getName());
+        }
     }
 }
--- Homework24/src/main/java/ua/training/model/Item.java
+++ Homework24/src/main/java/ua/training/model/Item.java
@@ -12,7 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 /**
- * Question: Here is an entity implementation and a UT that tests its functionality; Without changing the UT code, make this module build and UT cases pass properly.
+ * Task: Here is an entity implementation and a UT that tests its functionality; Without changing the UT code, make this module build and UT cases pass properly.
  * The UT cases tests for equality. First it checks that a similar, NOT SAME, Item should be asserted as equal because their field contents are the same.
  * The other case is testing its uniqueness in a collection, specifically in a HashSet... such that two Items that have the same values are treated
  * as equal so in a hashset only the first one inserted should exist.
